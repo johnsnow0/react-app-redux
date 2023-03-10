06a937ba9648c91f9f4aa6f9d8290e14d8debc08
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2023-03-10 12:49:59 +0200

    redux-test-complete-code-react

---
+++ src/state/store.js
@@ -0,0 +1,5 @@
+import reducers from "./reducers";
+import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
+export const store = configureStore({ reducer: reducers }, applyMiddleware(thunk))
